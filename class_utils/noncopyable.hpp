8a90206592959755b7159951ae8199448c6c5f44
cuiyajun <644583255@qq.com>
2016-12-06 09:31:50 +0800

    add cyj::noncopyable

---
+++ class_utils/noncopyable.hpp
@@ -0,0 +1,24 @@
+// noncopyable.hpp header file  Edit by cyj 2016.12.6
+
+// Private copy constructor and copy assignment ensure classes derived from
+// class noncopyable cannot be copied.
+// use a C++11 compiler
+
+#ifndef __NONCOPYABLE_HPP__
+#define __NONCOPYABLE_HPP__
+
+namespace cyj
+{
+	
+class noncopyable
+{
+protected:
+  noncopyable() = default;
+  ~noncopyable() = default;
+  noncopyable( const noncopyable& ) = delete;
+  noncopyable& operator=( const noncopyable& ) = delete;
+};
+
+}  // namespace cyj
+
+#endif  // __NONCOPYABLE_HPP__
