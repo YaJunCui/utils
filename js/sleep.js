1bae548417e9773322774f1620efe9f907d66a2b
YaJunCui <644583255@qq.com>
2017-09-26 08:42:57 +0800

    u
    a

--- js/sleep.js
+++ js/sleep.js
@@ -1,5 +1,5 @@
 // bad implementation
 function sleep(milliSeconds){
-	var startTime = new Date().getTime();                    // get the current time
-	while (new Date().getTime() < startTime + milliSeconds); // hog cpu
+  var startTime = new Date().getTime();                    // get the current time
+  while (new Date().getTime() < startTime + milliSeconds); // hog cpu
 }
